876ca0705315e290886e9560b7d9548b246ddf29
Harry <harryomollo95@gmail.com>
2020-10-19 23:28:54 +0300

    Debuging mode One

--- studioshto/settings.py
+++ studioshto/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0^!^d#o%x^efe4b$alnp6a#dpl^we#qyq@h^c$0!wk=8yi#d^^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['157.230.185.185','htostudios.com','www.htostudios.com',]
 
@@ -66,24 +66,17 @@ WSGI_APPLICATION = 'studioshto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'studioshto',
+    'USER': 'harry',
+    'PASSWORD': '$6085hto',
+    'HOST': 'localhost',
+    'PORT': '',
     }
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'studioshto',
-        'USER': 'harry',
-        'PASSWORD': '$6085hto',
-        'HOST': 'localhost',
-        'PORT': '',
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
